fc3094823ec657ffb7cf0b2e089e85bc0b851bf3
kiyoshi <shima.freebz@gmail.com>
2017-09-18 01:22:17 +0900

    いらないjsを削除

--- base/core/system.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var path = require("path");
-exports.system = {
-    ds: path.sep
-};
--- dist/base/core/system.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-let path = require("path");
-exports.system = {
-    ds: path.sep
-};
