492c3efec625c92d0459e76d590f33dde5681808
kiyoshi <shima.freebz@gmail.com>
2017-09-22 21:36:02 +0900

    test

--- dist/app.js
+++ dist/app.js
@@ -1,12 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const path = require("path");
@@ -25,9 +17,9 @@ const users_router_1 = require("./apps/users/users_router");
  */
 class main {
     constructor() {
-        this.ready = () => __awaiter(this, void 0, void 0, function* () {
+        this.ready = async () => {
             return this.app;
-        });
+        };
         this.create = () => {
             return this.app;
         };
--- dist/base/core/router.js
+++ dist/base/core/router.js
@@ -1,12 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const csurf = require("csurf");
 const inflection = require("inflection");
@@ -33,19 +25,19 @@ class router {
         this.beforeRender = (req, res) => {
         };
         this.loaders = [Promise.resolve];
-        this.loading = () => __awaiter(this, void 0, void 0, function* () {
+        this.loading = async () => {
             // helper loading
             let helpers = this.vars.hlp;
             for (var key in helpers) {
-                yield helpers[key].loading();
+                await helpers[key].loading();
             }
             //loaders loading;
             let loaders = this.loaders;
             for (var key in loaders) {
-                yield loaders[key];
+                await loaders[key];
             }
             return true;
-        });
+        };
         this._views = {
             common: "",
             typical: ""
