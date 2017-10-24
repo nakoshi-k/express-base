cc3cafa3d4a5fbfc423083f45b1ee9e7630a403e
nakashima kiyoshi <shima.freebz@gmail.com>
2017-10-24 19:09:42 +0900

    fix

--- apps/spa/auth/stores/mutations.ts
+++ apps/spa/auth/stores/mutations.ts
@@ -1,7 +1,11 @@
 import {mutations as core_mutations} from "../../../../base/spa/stores/mutations"
 
 export class mutations extends core_mutations{
-    constructor(options){
-      super()
-    }
+    
+  constructor(options){
+      super();
+  }
+
+  
+
 }
